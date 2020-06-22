6f26a3867ed4f58d29673e476dffec74a9eb5224
Joseph Hughes <Joseph.Hughes@version1.com>
2020-06-22 08:14:45 +0100

    wip all test working

--- src/main/java/com/lislea/tdd/greetingkata/Greeting.java
+++ src/main/java/com/lislea/tdd/greetingkata/Greeting.java
@@ -8,15 +8,15 @@ public class Greeting {
         String messageNormal = "Hello, %s.";
         String messageShout = "HELLO %s!";
         if (names == null) {
-            return String.format(messageNormal, "my friend");
+            return String.format("Hello, my friend.");
         }
         if (names.length > 1) {
             String message = "";
-            String titleCase = getListCsvListOfTitleNames(names);
+            String titleCase = getListCsvListOfNames(getTitleCaseNames(names));
             if (titleCase.length() > 0) {
                 message = message + String.format(messageNormal, titleCase);
             }
-            String upperCase = getListCsvListOfUpperNames(names);
+            String upperCase = getListCsvListOfNames(getListUpperNames(names));
             if (upperCase.length() > 0) {
                 if (message.length() > 0) {
                     message = message + " AND ";
@@ -32,43 +32,43 @@ public class Greeting {
 
     }
 
-    private String getListCsvListOfTitleNames(String ... names) {
-        List<String> titleCaseNames = new ArrayList<>();
-        for(String name:names){
-            if (!name.equals(name.toUpperCase()))
-                titleCaseNames.add(name);
-        }
+    private String getListCsvListOfNames(String ... names) {
 
-        if(titleCaseNames.size() == 0)
+        if(names.length == 0)
             return "";
 
-        String csv = String.join(", ", titleCaseNames);
+        String csv = String.join(", ", names);
         String and = ", and ";
         int numberOfCommas = csv.split(", ").length - 1;
         if (numberOfCommas == 0)
             return csv;
         if (numberOfCommas == 1)
             and = " and ";
-        return csv.substring(0, csv.lastIndexOf(", ")) + and + csv.substring(csv.lastIndexOf(", ") + 2);
+        return csv.substring(0, csv.lastIndexOf(", ")) + and + csv.substring(csv.lastIndexOf(", ") + 2)
+                .replaceAll("%", ",");
     }
 
-    private String getListCsvListOfUpperNames(String ... names) {
-        List<String> upperCaseNames = new ArrayList<>();
-        for(String name:names){
+    private String[] getListUpperNames(String ... names) {
+        List<String> nameList = new ArrayList<>();
+        for (String name : names) {
+            if(name.startsWith("\"") && name.endsWith("\"")){
+                name = name.replaceAll(",", "%");
+            }
             if (name.equals(name.toUpperCase()))
-                upperCaseNames.add(name);
+                nameList.add(name);
         }
+        return nameList.toArray(new String[nameList.size()]);
+    }
 
-        if(upperCaseNames.size() == 0)
-            return "";
-
-        String csv = String.join(", ", upperCaseNames);
-        String and = ", AND ";
-        int numberOfCommas = csv.split(", ").length - 1;
-        if (numberOfCommas == 0)
-            return csv;
-        if (numberOfCommas == 1)
-            and = " AND ";
-        return csv.substring(0, csv.lastIndexOf(", ")) + and + csv.substring(csv.lastIndexOf(", ") + 2);
+    private String[] getTitleCaseNames(String ... names){
+        List<String> nameList = new ArrayList<String>();
+        for(String name:names){
+            if(name.startsWith("\"") && name.endsWith("\"")){
+                name = name.replaceAll(",", "%").replaceAll("\"", "");
+            }
+            if (!name.equals(name.toUpperCase()))
+                nameList.add(name);
+        }
+        return nameList.toArray(new String[nameList.size()]);
     }
 }
